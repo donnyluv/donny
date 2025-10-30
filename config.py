dbfe7059b1fc03ef7a513fa6f3f0a58f541912ef
donnyluv <maksimokorokov76@gmail.com>
2025-10-30 22:44:28 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+"""Конфигурационный файл для хранения секретов и настроек бота."""
+
+# Токен бота Discord. Замените строку ниже на реальный токен вашего бота.
+TOKEN = "MTMxOTI5Mzg0MzU0MzQyOTE1MA.GZCHKU.xX91GJAhRHYFuH4I5ZMurO759OEyj9lr5MN_Ik"
